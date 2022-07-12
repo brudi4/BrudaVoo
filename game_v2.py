33014565e53ef064e7db479ad8e74750a3676bf3
BrudaVoo <brudavoo@gmail.com>
2022-07-12 12:34:48 +0300

    Initial commit

---
+++ game_v2.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def random_predict(number):
+    counter = 0
+    while True:
+        num = np.random.randint(1,10)
+        counter += 1
+        if num == number:
+            break
+    return counter
+
+def score_game(random_predict):
+    counters_list = []
+    nums_to_guess = np.random.randint(1,10, size=(50))
+    for i in nums_to_guess:
+        counters_list.append(random_predict(i))
+    srednee = np.mean(counters_list)
+    return srednee
+
+print(f'Srednee kolichestvo popitok ugadivaniya: {score_game(random_predict)}')
+
+
+#if __name__ == '__main__':
+    #score_game(random_predict)
